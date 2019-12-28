56311a69999478a57b611cea17d996b2664a7ff1
fahad9601 <fahad9601@gmail.com>
2019-12-28 19:05:43 +0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CopyrightComponent } from './copyright/copyright.component';
+import { FaqComponent } from './faq/faq.component';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { TermsComponent } from './terms/terms.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  //, component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'news', component: NewsComponent},
+  {path: 'faq', component: FaqComponent},
+  {path: 'copyright', component: CopyrightComponent},
+  {path: 'privacy', component: PrivacyComponent},
+  {path: 'terms', component: TermsComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule,
+   MatButtonModule,
+   MatToolbarModule,
+    MatExpansionModule, MatIconModule, MatSidenavModule
+  } from '@angular/material';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FaqComponent } from './faq/faq.component';
+import { NewsComponent } from './news/news.component';
+import { TermsComponent } from './terms/terms.component';
+import { CopyrightComponent } from './copyright/copyright.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaqComponent,
+    NewsComponent,
+    TermsComponent,
+    CopyrightComponent,
+    PrivacyComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
